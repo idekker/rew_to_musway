6203105d2fdb7212f9c0b70c943bb30fd6ed2052
Ino Dekker <ino.dekker@gmail.com>
2026-04-27 09:51:46 +0200

    Remove unused eq and verification functions

--- rew_to_musway/calibration/__init__.py
+++ rew_to_musway/calibration/__init__.py
@@ -1,7 +1,7 @@
 """calibration - Calibration phase implementations."""
 
 from ._combined import run_combined_measurements
-from ._eq import calibrate_channels, select_channels
+from ._eq import select_channels
 from ._levels import LevelOffsets
 from ._unified import (
     MeasureResult,
@@ -11,18 +11,16 @@ from ._unified import (
     run_measure_loop,
     run_verification_loop,
 )
-from ._verification import run_verification, save_session
+from ._verification import save_session
 
 __all__ = [
     "LevelOffsets",
     "MeasureResult",
     "VerificationResult",
     "_eligible_finetune_channels",
-    "calibrate_channels",
     "run_combined_measurements",
     "run_finetune_loop",
     "run_measure_loop",
-    "run_verification",
     "run_verification_loop",
     "save_session",
     "select_channels",
